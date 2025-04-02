3ba83cfbdd5837b2de9ffa140a7073214079b46c
BrandGerry <91706030+BrandGerry@users.noreply.github.com>
2025-04-02 10:05:24 -0600

    ADD:first commit

---
+++ src/views/IndesicionView.vue
@@ -0,0 +1,17 @@
+<!-- Fuente: https://tailwindcomponents.com/component/chat-layout -->
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Mi esposa</span>
+    </div>
+    <ChatMessages :messages="messages" />
+    <MessageBox @send-message="onMessage" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useChat } from '@/components/useChat';
+import ChatMessages from '../chat/ChatMessages.vue';
+import MessageBox from '../chat/MessageBox.vue';
+const { messages, onMessage } = useChat();
+</script>
